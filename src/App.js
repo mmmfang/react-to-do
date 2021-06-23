18bb90c57eebbf59273866425750be68ba1eea35
Mae Fang <+>
2021-06-23 18:23:53 -0400

    map out todos data and add to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 //import logo from './logo.svg';
 import './App.css';
 import ToDoItem from './components/ToDoItem'
+import todosData from './todosData.js'
 
 function App() {
+  const toDosArray = todosData.map(todo => 
+    <ToDoItem
+      key = {todo.id}
+      text = {todo.text}
+      completed = {todo.completed}
+    />)
   return (
     <div className="App-header">
       <div className="to-do-list-bg">
-        <ToDoItem />
-        <ToDoItem />
-        <ToDoItem />
-        <ToDoItem />
+        {toDosArray}
       </div>
     </div>
   );
