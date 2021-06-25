0836911279264b8ed478f813dff4d853900f282b
Mae Fang <+>
2021-06-24 21:35:59 -0400

    switched to class based component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 //import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 import ToDoItem from './components/ToDoItem'
 import todosData from './todosData.js'
 
-function App() {
-  const toDosArray = todosData.map(todo => 
-    <ToDoItem
-      key = {todo.id}
-      todo = {todo}
-      // text = {todo.text}
-      // completed = {todo.completed}
-    />)
-  return (
-    <div className="App-header">
-      <div className="to-do-list-bg">
-        {toDosArray}
+class App extends Component {
+
+  render(){
+
+    const toDosArray = todosData.map(todo => 
+      <ToDoItem
+        key = {todo.id}
+        todo = {todo}
+        // text = {todo.text}
+        // completed = {todo.completed}
+      />)
+
+    return (
+      <div className="App-header">
+        <div className="to-do-list-bg">
+          {toDosArray}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
